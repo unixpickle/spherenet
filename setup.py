7bd37794d22147dc0889afbdcde82f1e1221a960
Alex Nichol <unixpickle@gmail.com>
2017-11-24 11:31:52 -0500

    latest version on PyPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='spherenet',
-    version='0.0.2',
+    version='0.0.3',
     description='Deep Hyperspherical Learning.',
     long_description='Implementation of https://arxiv.org/abs/1711.03189v1.',
     url='https://github.com/unixpickle/spherenet',
